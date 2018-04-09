fa10558c1c00de506be104274675294f3f6e3524
jcdebroeck <jcdebroeck@hotmail.com>
2018-04-08 17:52:42 -0700

    reorganization

--- MenuSample/Properties/AssemblyInfo.cs
+++ MenuSample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("ConsoleAppTest")]
+[assembly: AssemblyTitle ("MenuSample")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("ConsoleAppTest")]
+[assembly: AssemblyProduct ("MenuSample")]
 [assembly: AssemblyCopyright ("Copyright ©  2018")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
